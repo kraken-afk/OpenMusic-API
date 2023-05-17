b174c43c243f495ac0e626680879a4c0ce74766c
Romeo Noveanre <noveanrer@gmail.com>
2023-05-17 13:18:05 +0700

    add update feature to albums

--- src/models/AlbumsModel.ts
+++ src/models/AlbumsModel.ts
@@ -1,10 +1,20 @@
-import { PrismaClient } from '@prisma/client';
-import { Album, AlbumsCreation, DataAlbumCreated, DatabaseResponseNegative, DatabaseResponsePositive } from '../app.d';
-import PrismaScheme from '../config/PrismaScheme';
-import shortid from 'shortid';
+import { PrismaClient } from "@prisma/client";
+import {
+  Album,
+  AlbumsCreation,
+  DataAlbumCreated,
+  DatabaseResponseNegative,
+  DatabaseResponsePositive,
+} from "../app.d";
+import PrismaScheme from "../config/PrismaScheme";
+import shortid from "shortid";
 
 export default abstract class AlbumsModel {
-  static async create(data: AlbumsCreation): Promise<DatabaseResponsePositive<DataAlbumCreated> | DatabaseResponseNegative> {
+  static async create(
+    data: AlbumsCreation
+  ): Promise<
+    DatabaseResponsePositive<DataAlbumCreated> | DatabaseResponseNegative
+  > {
     const prisma = new PrismaClient(PrismaScheme);
     const id = shortid.generate();
 
@@ -13,8 +23,8 @@ export default abstract class AlbumsModel {
       const response: DatabaseResponsePositive<DataAlbumCreated> = {
         status: true,
         data: {
-          albumId: id
-        }
+          albumId: id,
+        },
       };
 
       return response;
@@ -22,7 +32,8 @@ export default abstract class AlbumsModel {
       console.error(error);
       const response: DatabaseResponseNegative = {
         status: false,
-        message: error.message ?? 'Unable to create album, something went Error!'
+        message:
+          error?.message ?? "Unable to create album, something went Error!",
       };
 
       return response;
@@ -31,12 +42,40 @@ export default abstract class AlbumsModel {
     }
   }
 
-  static async get(id: string): Promise<Album | null>  {
+  static async get(id: string): Promise<Album | null> {
     const prisma = new PrismaClient(PrismaScheme);
     const album: Album | null = await prisma.albums.findUnique({
-      where: { id }
+      where: { id },
     });
     prisma.$disconnect();
     return album;
+  }
+
+  static async update(
+    id: string,
+    { name, year }: AlbumsCreation
+  ): Promise<
+    DatabaseResponsePositive<{ message: string }> | DatabaseResponseNegative
+  > {
+    const prisma = new PrismaClient(PrismaScheme);
+    try {
+      await prisma.albums.update({
+        where: { id },
+        data: { name, year },
+      });
+      const response: DatabaseResponsePositive<{ message: string }> = {
+        status: true,
+        data: { message: `Album of id: '${id} has been updated.'` },
+      };
+      return response;
+    } catch (error) {
+      const response: DatabaseResponseNegative = {
+        status: false,
+        message: error?.message ?? "Album not found",
+      };
+      return response;
+    } finally {
+      prisma.$disconnect();
+    }
   }
 }
--- src/routes/albums.ts
+++ src/routes/albums.ts
@@ -1,61 +1,108 @@
-import { Router, type Response, type Request } from 'express';
-import validateAlbumCreation from '../validators/albumsValidator';
-import { AlbumsCreation, AlbumsResponse } from '../app.d';
-import AlbumsModel from '../models/AlbumsModel';
+import { Router, type Response, type Request } from "express";
+import validateAlbumCreation from "../validators/albumsValidator";
+import { AlbumsCreation, AlbumsResponse } from "../app.d";
+import AlbumsModel from "../models/AlbumsModel";
 
 const router = Router();
 
-router.get('/:id', async (req: Request, res: Response): Promise<void> => {
+router.get("/:id", async (req: Request, res: Response): Promise<void> => {
   const { id } = req.params;
   const album = await AlbumsModel.get(id);
-  let response: AlbumsResponse
+  let response: AlbumsResponse;
 
   if (!album)
-    (response = {
-      status: 'fail',
+    response = {
+      status: "fail",
       code: 404,
-      message: 'Album not found',
-    });
+      message: "Album not found",
+    };
   else
-    (response = {
-      status: 'success',
+    response = {
+      status: "success",
       code: 200,
       data: {
         album,
-      }
-    });
+      },
+    };
 
-  res.setHeader('Content-Type', 'application/json')
-    .setHeader('Content-Length', Buffer.byteLength(JSON.stringify(response), 'utf-8'))
+  res
+    .setHeader("Content-Type", "application/json")
+    .setHeader(
+      "Content-Length",
+      Buffer.byteLength(JSON.stringify(response), "utf-8")
+    )
     .status(response.code)
     .send(response);
 });
 
-router.post('/', validateAlbumCreation, async (req: Request, res: Response): Promise<void> => {
-  const album: AlbumsCreation = req.body;
-  const dbResponse = await AlbumsModel.create(album);
-  let response: AlbumsResponse;
+router.put(
+  "/:id",
+  validateAlbumCreation,
+  async (req: Request, res: Response): Promise<void> => {
+    const { id } = req.params;
+    const { name, year } = req.body;
+    const { status } = await AlbumsModel.update(id, { name, year });
+    let response: AlbumsResponse;
 
-  if (!dbResponse.status)
-    (response = {
-      status: 'fail',
-      code: 500,
-      message: 'Internal server error',
-    });
-  else
-    (response = {
-      status: 'success',
-      code: 201,
-      data: {
-        albumId: dbResponse.data.albumId,
-      }
-    });
+    console.log(status);
 
-  res.setHeader('Content-Type', 'application/json')
-    .setHeader('Content-Length', Buffer.byteLength(JSON.stringify(response), 'utf-8'))
-    .status(response.code)
-    .send(response);
-});
+    if (! status )
+      response = {
+        status: "fail",
+        code: 404,
+        message: "Album not found",
+      };
+    else
+      response = {
+        status: "success",
+        code: 200,
+        message: "Album has been updated",
+      };
+
+    res
+      .setHeader("Content-Type", "application/json")
+      .setHeader(
+        "Content-Length",
+        Buffer.byteLength(JSON.stringify(response), "utf-8")
+      )
+      .status(response.code)
+      .send(response);
+  }
+);
+
+router.post(
+  "/",
+  validateAlbumCreation,
+  async (req: Request, res: Response): Promise<void> => {
+    const album: AlbumsCreation = req.body;
+    const dbResponse = await AlbumsModel.create(album);
+    let response: AlbumsResponse;
+
+    if (!dbResponse.status)
+      response = {
+        status: "fail",
+        code: 500,
+        message: "Internal server error",
+      };
+    else
+      response = {
+        status: "success",
+        code: 201,
+        data: {
+          albumId: dbResponse.data.albumId,
+        },
+      };
+
+    res
+      .setHeader("Content-Type", "application/json")
+      .setHeader(
+        "Content-Length",
+        Buffer.byteLength(JSON.stringify(response), "utf-8")
+      )
+      .status(response.code)
+      .send(response);
+  }
+);
 
 const AlbumRouter = router;
 export default AlbumRouter;
